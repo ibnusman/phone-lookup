83d67dea42177cf1c68d96fb937d8a75327991ed
ibnusman <abusaleh3035@gmail.com>
2025-04-19 17:40:03 +0100

    Fix favicon, analytics cookie warning, and prepare for Vercel deployment

--- src/analytics.js
+++ src/analytics.js
@@ -3,6 +3,7 @@ import ReactGA from 'react-ga4';
 const initializeAnalytics = () => {
   ReactGA.initialize('G-6PWL9BLJ8G'); // Replace with your GA4 Measurement ID
   // Manually track the page view
+  
   ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
 };
 
