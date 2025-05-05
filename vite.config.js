c021cc6845ef06f674ab3a05d1d4327e9744effa
ibnusman <abusaleh3035@gmail.com>
2025-05-05 12:56:50 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(),],
   css: {
-    postcss: {}  // You don't need to specify the path to postcss.config.js here
+    postcss: {}  
   }
 })
